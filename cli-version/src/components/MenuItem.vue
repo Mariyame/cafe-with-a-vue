708585b72574537e8f347063f5188b25f61ca69e
Mariam <mariam.chemrah@gmail.com>
2021-09-15 16:28:42 +0100

    B3C1-Begin Exercice

--- cli-version/src/components/MenuItem.vue
+++ cli-version/src/components/MenuItem.vue
@@ -1,7 +1,27 @@
 <script>
 export default {
 	name: "MenuItem",
-	props: ["addToShoppingCart", "image", "inStock", "name", "quantity"]
+	props: ["addToShoppingCart", "image", "inStock", "name", "price", "quantity"],
+	data() {
+		return {
+			onSale: false
+		}
+	},
+	computed: {
+		generatedPrice() {
+			if (this.onSale) {
+				return (this.price * 0.9).toFixed(2)
+			} else {
+				return this.price
+			}
+		}
+	},
+	beforeMount() {
+		const today = new Date().getDate()
+		if (today % 2 === 0) {
+			this.onSale = true
+		}
+	}
 }
 </script>
 
@@ -10,11 +30,12 @@ export default {
 		<img class="menu-item__image" :src="image.source" :alt="image.alt" />
 		<div>
 			<h3>{{ name }}</h3>
+			<p>Prix : {{ generatedPrice }}</p>
 			<p v-if="inStock">En stock</p>
 			<p v-else>En rupture de stock</p>
 			<div>
 				<label for="add-item-quantity">Quantité : {{ quantity }}</label>
-				<input v-model.number="quantity" id="add-item-quantity" type="number" />
+				<input  id="add-item-quantity" type="number" />
 				<button @click="addToShoppingCart(quantity)">
 					Ajouter au panier
 				</button>
@@ -23,4 +44,16 @@ export default {
 	</div>
 </template>
 
-<style></style>
+<style>
+
+  .menu-item {
+    display: flex;
+    width: 500px;
+    justify-content: space-between;
+    margin-bottom: 30px;
+  }
+  
+  .menu-item__image {
+    max-width: 300px;
+  }
+</style>
